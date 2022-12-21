d215e516d872cda4d8cef0bec32bf6085c040065
gracentruth <63465350+gracentruth@users.noreply.github.com>
2022-12-21 11:42:58 +0900

    12/21 수정

--- lib/screen/Camera/camera_bndbox.dart
+++ lib/screen/Camera/camera_bndbox.dart
@@ -1,4 +1,5 @@
 import 'dart:typed_data';
+
 import 'package:camera/camera.dart';
 import 'package:firebase_storage/firebase_storage.dart';
 import 'package:flutter/material.dart';
@@ -26,48 +27,48 @@ class BndBox extends StatefulWidget {
 
 class _BndBoxState extends State<BndBox> {
   @override
-  Future<void> takepicture() async {
-    setState(() {});
-    CameraImage image = cameraImage2;
-    //String object2=object;
-    try {
-      final int width = image.width;
-      final int height = image.height;
-      final int uvRowStride = image.planes[1].bytesPerRow;
-      final int uvPixelStride = image.planes[1].bytesPerPixel!;
-      print("uvRowStride: " + uvRowStride.toString());
-      print("uvPixelStride: " + uvPixelStride.toString());
-      var img = imglib.Image(width, height); // Create Image buffer
-      for (int x = 0; x < width; x++) {
-        for (int y = 0; y < height; y++) {
-          final int uvIndex =
-              uvPixelStride * (x / 2).floor() + uvRowStride * (y / 2).floor();
-          final int index = y * width + x;
-          final yp = image.planes[0].bytes[index];
-          final up = image.planes[1].bytes[uvIndex];
-          final vp = image.planes[2].bytes[uvIndex];
-          int r = (yp + vp * 1436 / 1024 - 179).round().clamp(0, 255);
-          int g = (yp - up * 46549 / 131072 + 44 - vp * 93604 / 131072 + 91)
-              .round()
-              .clamp(0, 255);
-          int b = (yp + up * 1814 / 1024 - 227).round().clamp(0, 255);
-          img.data[index] = (0xFF << 24) | (b << 16) | (g << 8) | r;
-        }
-      }
-
-      imglib.PngEncoder pngEncoder = new imglib.PngEncoder(level: 0, filter: 0);
-      List<int> png = pngEncoder.encodeImage(img);
-
-      final uploadTask = await storage
-          // .ref('tflitetest/${object2}/${object2}${DateTime.now()}.png')
-          .ref('tflitetest/hello/test.png')
-          .putData(Uint8List.fromList(png));
-      final url = await uploadTask.ref.getDownloadURL();
-      print(url);
-    } catch (e) {
-      print(">>>>>>>>>>>> ERROR:" + e.toString());
-    }
-  }
+  // Future<void> takepicture() async {
+  //   setState(() {});
+  //   CameraImage image = cameraImage2;
+  //   //String object2=object;
+  //   try {
+  //     final int width = image.width;
+  //     final int height = image.height;
+  //     final int uvRowStride = image.planes[1].bytesPerRow;
+  //     final int uvPixelStride = image.planes[1].bytesPerPixel!;
+  //     print("uvRowStride: " + uvRowStride.toString());
+  //     print("uvPixelStride: " + uvPixelStride.toString());
+  //     var img = imglib.Image(width, height); // Create Image buffer
+  //     for (int x = 0; x < width; x++) {
+  //       for (int y = 0; y < height; y++) {
+  //         final int uvIndex =
+  //             uvPixelStride * (x / 2).floor() + uvRowStride * (y / 2).floor();
+  //         final int index = y * width + x;
+  //         final yp = image.planes[0].bytes[index];
+  //         final up = image.planes[1].bytes[uvIndex];
+  //         final vp = image.planes[2].bytes[uvIndex];
+  //         int r = (yp + vp * 1436 / 1024 - 179).round().clamp(0, 255);
+  //         int g = (yp - up * 46549 / 131072 + 44 - vp * 93604 / 131072 + 91)
+  //             .round()
+  //             .clamp(0, 255);
+  //         int b = (yp + up * 1814 / 1024 - 227).round().clamp(0, 255);
+  //         img.data[index] = (0xFF << 24) | (b << 16) | (g << 8) | r;
+  //       }
+  //     }
+  //
+  //     imglib.PngEncoder pngEncoder = new imglib.PngEncoder(level: 0, filter: 0);
+  //     List<int> png = pngEncoder.encodeImage(img);
+  //
+  //     final uploadTask = await storage
+  //         // .ref('tflitetest/${object2}/${object2}${DateTime.now()}.png')
+  //         .ref('tflitetest/hello/test.png')
+  //         .putData(Uint8List.fromList(png));
+  //     final url = await uploadTask.ref.getDownloadURL();
+  //     print(url);
+  //   } catch (e) {
+  //     print(">>>>>>>>>>>> ERROR:" + e.toString());
+  //   }
+  // }
 
   FirebaseStorage storage = FirebaseStorage.instance;
 
