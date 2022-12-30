d251b3088fccf740b617a9d8505b8e06cb828561
gracentruth <63465350+gracentruth@users.noreply.github.com>
2022-12-30 18:58:09 +0900

    12/30 수정4

--- lib/screen/album/catchbox.dart
+++ lib/screen/album/catchbox.dart
@@ -60,7 +60,7 @@ class _CatchboxState extends State<Catchbox> {
           padding: EdgeInsets.zero,
           icon: Image.asset("assets/icons/icon_back.png"),
           onPressed: (){
-         //   Get.back();
+            //   Get.back();
           },
         ),
         centerTitle: true,
@@ -73,7 +73,7 @@ class _CatchboxState extends State<Catchbox> {
           children: [
             Expanded(
                 child: StreamBuilder<QuerySnapshot>(
-                    stream: FirebaseFirestore.instance.collection('category').doc(FirebaseAuth.instance.currentUser!.email).collection('category').orderBy('category', descending: true).snapshots(),
+                    stream: FirebaseFirestore.instance.collection('category').doc('1234@handong.ac.kr').collection('category').orderBy('category', descending: true).snapshots(),
                     builder: (context, snapshot){
                       if(snapshot.hasData){
                         return Container(
